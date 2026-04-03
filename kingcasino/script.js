8355af0e806b00e5ee0ebbe6a0dcb66d207c7fe4
Narek Hovsepyan <narekhovsepyan@Nareks-MacBook-Pro.local>
2026-04-03 20:45:50 +0400

    Refactor kingcasino theme to actual DOM

--- kingcasino/script.js
+++ kingcasino/script.js
@@ -40,8 +40,47 @@
     document.body.setAttribute("data-kingcasino-theme", "stake");
   }
 
+  function annotateLayout() {
+    var header = document.querySelector('[aria-label="site-header"]');
+    if (header) {
+      header.setAttribute("data-kc-header", "true");
+
+      var headerInner = header.parentElement;
+      if (headerInner) {
+        headerInner.setAttribute("data-kc-header-inner", "true");
+      }
+
+      var menuButton = header.querySelector('button[aria-label="menu"]');
+      if (menuButton) menuButton.setAttribute("data-kc-menu-button", "true");
+
+      var logoLink = header.querySelector('a[href="/en/"]');
+      if (logoLink) logoLink.setAttribute("data-kc-logo", "true");
+
+      var promoLink = header.querySelector('a[href="/en/promotions"]');
+      if (promoLink) promoLink.setAttribute("data-kc-promo-link", "true");
+
+      var loginLink = header.querySelector('a[href*="?m=login"]');
+      if (loginLink) loginLink.setAttribute("data-kc-login", "true");
+
+      var registerLink = header.querySelector('a[href*="?m=registration"]');
+      if (registerLink) registerLink.setAttribute("data-kc-register", "true");
+
+      var leftBlock = menuButton && menuButton.parentElement;
+      if (leftBlock) leftBlock.setAttribute("data-kc-header-left", "true");
+
+      var rightBlock = registerLink && registerLink.parentElement && registerLink.parentElement.parentElement;
+      if (rightBlock) rightBlock.setAttribute("data-kc-header-right", "true");
+    }
+
+    var footer = document.querySelector("footer");
+    if (footer) {
+      footer.setAttribute("data-kc-footer", "true");
+    }
+  }
+
   function runEnhancements() {
     addBodyFlag();
+    annotateLayout();
     setSectionIdByText("kingcasino-originals", "Original");
     setSectionIdByText("kingcasino-sports", "Sport");
     setSectionIdByText("kingcasino-live", "Live");
