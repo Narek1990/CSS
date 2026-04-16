8f2d18b25d1140232fc5f74cbde8a00df723b1ee
Narek Hovsepyan <narekhovsepyan@Nareks-MacBook-Pro.local>
2026-04-16 11:01:06 +0400

    Update esportesnew menu icon overrides

--- esportesnew/script.js
+++ esportesnew/script.js
@@ -14,6 +14,9 @@
   var narrowContainerSelector = ".css-11xzi44 .css-17u1px6";
   var wideContainerIconSelector = ".css-11xzi44 .css-1huuf1k .sl-icon.css-1nqq47m";
   var shadowContainerSelector = ".css-fkpkqq .css-1pyebjd";
+  var topContainerSelector = ".css-0 .css-10a4mqi";
+  var menuIconSelector = ".sl-icon.css-17sgcqa";
+  var menuSvg = '<svg data-esportesnew-svg="true" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" height="14" viewBox="0 0 19 14" width="19" xmlns="http://www.w3.org/2000/svg"><path d="M1.26049 13.5939H8.30622C8.86214 13.5939 9.31284 13.1433 9.31284 12.5874C9.31284 12.0315 8.86214 11.5808 8.30622 11.5808H1.26049C0.70458 11.5808 0.253944 12.0315 0.253944 12.5874C0.253944 13.1433 0.70458 13.5939 1.26049 13.5939ZM1.26049 8.17949H17.365C17.9209 8.17949 18.3715 7.72887 18.3715 7.17296C18.3715 6.61704 17.9209 6.16642 17.365 6.16642H1.26045C0.704542 6.16642 0.253906 6.61704 0.253906 7.17296C0.253906 7.72887 0.70458 8.17949 1.26049 8.17949ZM1.26049 2.76505H17.365C17.9209 2.76505 18.3715 2.31441 18.3715 1.7585C18.3715 1.20259 17.9209 0.751953 17.365 0.751953H1.26045C0.704542 0.751953 0.253906 1.20259 0.253906 1.7585C0.253906 2.31441 0.70458 2.76505 1.26049 2.76505Z" fill="#E8E5FF"></path></svg>';
 
   function applyOverrides() {
     document.querySelectorAll(maxWidthSelector).forEach(function (element) {
@@ -45,6 +48,20 @@
 
     document.querySelectorAll(shadowContainerSelector).forEach(function (element) {
       element.style.setProperty("box-shadow", "none", "important");
+    });
+
+    document.querySelectorAll(topContainerSelector).forEach(function (element) {
+      element.style.setProperty("width", "180px", "important");
+      element.style.setProperty("margin-left", "48px", "important");
+    });
+
+    document.querySelectorAll(menuIconSelector).forEach(function (element) {
+      element.style.setProperty("right", "201px", "important");
+      element.style.setProperty("top", "19px", "important");
+
+      if (!element.querySelector('svg[data-esportesnew-svg="true"]')) {
+        element.innerHTML = menuSvg;
+      }
     });
   }
 
