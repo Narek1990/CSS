9152b5a6ab11734d854eea56e778ddb3d3654c12
Narek Hovsepyan <narekhovsepyan@Nareks-MacBook-Pro.local>
2026-04-18 10:15:24 +0400

    Add kingcasino button SVG override

--- kingcasino/script.js
+++ kingcasino/script.js
@@ -8,17 +8,78 @@
 
   var href = baseUrl + "/kingcasino.css?v=" + Date.now();
   var existing = document.querySelector("link[data-kingcasino-css=\"true\"]");
+  var buttonIconSelector = "button.sl-icon.css-1tev38v, button .sl-icon.css-1tev38v, .sl-icon.css-1tev38v svg";
+  var buttonIconSvg = "<svg data-kingcasino-button-svg=\"true\" fill=\"none\" height=\"14\" viewBox=\"0 0 19 14\" width=\"19\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1.26049 13.5939H8.30622C8.86214 13.5939 9.31284 13.1433 9.31284 12.5874C9.31284 12.0315 8.86214 11.5808 8.30622 11.5808H1.26049C0.70458 11.5808 0.253944 12.0315 0.253944 12.5874C0.253944 13.1433 0.70458 13.5939 1.26049 13.5939ZM1.26049 8.17949H17.365C17.9209 8.17949 18.3715 7.72887 18.3715 7.17296C18.3715 6.61704 17.9209 6.16642 17.365 6.16642H1.26045C0.704542 6.16642 0.253906 6.61704 0.253906 7.17296C0.253906 7.72887 0.70458 8.17949 1.26049 8.17949ZM1.26049 2.76505H17.365C17.9209 2.76505 18.3715 2.31441 18.3715 1.7585C18.3715 1.20259 17.9209 0.751953 17.365 0.751953H1.26045C0.704542 0.751953 0.253906 1.20259 0.253906 1.7585C0.253906 2.31441 0.70458 2.76505 1.26049 2.76505Z\" fill=\"#E8E5FF\"></path></svg>";
+  var applyScheduled = false;
 
-  if (existing) {
-    existing.href = href;
-    return;
+  function ensureStylesheet() {
+    if (existing) {
+      existing.href = href;
+      return;
+    }
+
+    document.head.appendChild(
+      Object.assign(document.createElement("link"), {
+        rel: "stylesheet",
+        href: href,
+        "data-kingcasino-css": "true"
+      })
+    );
   }
 
-  document.head.appendChild(
-    Object.assign(document.createElement("link"), {
-      rel: "stylesheet",
-      href: href,
-      "data-kingcasino-css": "true"
-    })
-  );
+  function createButtonIconSvg() {
+    var template = document.createElement("template");
+    template.innerHTML = buttonIconSvg;
+    return template.content.firstElementChild;
+  }
+
+  function replaceButtonIconSvg(element) {
+    var targetSvg = element.matches("svg") ? element : element.querySelector("svg");
+    var replacement;
+
+    if (targetSvg && targetSvg.getAttribute("data-kingcasino-button-svg") === "true") {
+      return;
+    }
+
+    replacement = createButtonIconSvg();
+
+    if (targetSvg) {
+      replacement.setAttribute("class", targetSvg.getAttribute("class") || "");
+      targetSvg.replaceWith(replacement);
+      return;
+    }
+
+    element.appendChild(replacement);
+  }
+
+  function applyOverrides() {
+    applyScheduled = false;
+    document.querySelectorAll(buttonIconSelector).forEach(replaceButtonIconSvg);
+  }
+
+  function scheduleApplyOverrides() {
+    if (applyScheduled) {
+      return;
+    }
+
+    applyScheduled = true;
+    requestAnimationFrame(applyOverrides);
+  }
+
+  function init() {
+    ensureStylesheet();
+    applyOverrides();
+    new MutationObserver(scheduleApplyOverrides).observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class", "style"],
+      childList: true,
+      subtree: true
+    });
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init, { once: true });
+  } else {
+    init();
+  }
 })();
