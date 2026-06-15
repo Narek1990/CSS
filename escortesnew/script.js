cdbc2414b3a7496593a0cf07b358ca01146bd33f
Narek Hovsepyan <narekhovsepyan@Nareks-MacBook-Pro.local>
2026-06-16 00:01:54 +0400

    Add escortesnew footer assets

---
+++ escortesnew/script.js
@@ -0,0 +1,55 @@
+(function () {
+  var currentScript = document.currentScript && document.currentScript.src;
+  var ASSET_BASE = currentScript
+    ? currentScript.replace(/\/script\.js(?:\?.*)?$/, "")
+    : "https://cdn.jsdelivr.net/gh/Narek1990/CSS@refs/heads/main/escortesnew";
+  var CSS_HREF = ASSET_BASE + "/escortesnew.css?v=" + Date.now();
+
+  function ensureCss() {
+    if (document.querySelector('link[data-escortesnew-css="true"]')) return;
+
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = CSS_HREF;
+    link.setAttribute("data-escortesnew-css", "true");
+    document.head.appendChild(link);
+  }
+
+  function buildImage(src, alt, key) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.alt = alt;
+    img.loading = "lazy";
+    img.decoding = "async";
+    img.setAttribute("data-escortesnew-footer-img", key);
+    return img;
+  }
+
+  function ensureFooterAssets() {
+    var footer = document.querySelector('[data-mj="footer"]');
+    var content = footer && footer.querySelector('[data-mj="footer-content"]');
+    if (!content || content.querySelector('[data-escortesnew-footer-assets="true"]')) return;
+
+    var target = content.querySelector(".app-ltr-19zdw54, [class~='app-ltr-19zdw54']");
+    var wrap = document.createElement("div");
+    wrap.className = "escortesnew-footer-assets";
+    wrap.setAttribute("data-escortesnew-footer-assets", "true");
+
+    wrap.appendChild(buildImage(ASSET_BASE + "/footer/anjouan-logo.png", "Anjouan license", "anjouan"));
+    wrap.appendChild(buildImage(ASSET_BASE + "/footer/ios-img.svg", "Download on iOS", "ios"));
+    wrap.appendChild(buildImage(ASSET_BASE + "/footer/android-img.svg", "Download on Android", "android"));
+
+    content.insertBefore(wrap, target || null);
+  }
+
+  function boot() {
+    ensureCss();
+    ensureFooterAssets();
+  }
+
+  boot();
+  new MutationObserver(boot).observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+})();
